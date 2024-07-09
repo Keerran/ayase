9f6dd73d6060bb94acc95e9947caa4b6ad7704cb
Keerran <kierankumar@gmail.com>
2024-07-09 01:13:05 +0100

    feat: move main.py code to main function

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
+import asyncio
 from ayase.bot import Bot
 from dotenv import load_dotenv
 
-load_dotenv()
-bot = Bot()
-bot.run(os.getenv("DISCORD_TOKEN"))
+
+async def main():
+    load_dotenv()
+    bot = Bot()
+    await bot.start(os.getenv("DISCORD_TOKEN"))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
