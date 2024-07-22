3e698947dd5945fac59cedf55d2f2bb9b649d38b
Keerran <kierankumar@gmail.com>
2024-07-22 20:14:52 +0100

    feat: add SQL file to reverse certain names

---
+++ reverse_name.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE FUNCTION array_reverse(anyarray) RETURNS anyarray AS $$
+SELECT ARRAY(
+    SELECT $1[i]
+    FROM generate_subscripts($1,1) AS s(i)
+    ORDER BY i DESC
+);
+$$ LANGUAGE 'sql' STRICT IMMUTABLE;
+
+UPDATE characters
+    SET name = array_to_string(array_reverse(string_to_array(name, ' ')), ' ')
+    FROM medias
+    WHERE characters.media_id = medias.id
+    AND medias.title = 'One Piece';
